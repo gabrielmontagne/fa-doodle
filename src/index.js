c8e9854df823278783e7e7662374a7f794814162
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-02 16:19:10 +0000

    install rxjs

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import Timeline from './Timeline'
+import './services'
 
 ReactDOM.render(<Timeline />, document.getElementById('root'))
