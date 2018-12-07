5efecaa4c2ab1dabd78ea31e6e8af0e003ec8648
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-07 23:54:42 +0000

    fix rotation

--- src/Model.js
+++ src/Model.js
@@ -1,53 +1,76 @@
 import React from 'react'
+import createTransition$ from './transition'
 import sizeMe from 'react-sizeme'
 import style from './Model.module.css'
+import log from 'caballo-vivo/src/log'
 import toFloat from './to-float'
-import { BoxGeometry, Mesh, MeshBasicMaterial, PerspectiveCamera, Scene, WebGLRenderer, } from 'three'
-import { pick, compose } from 'ramda'
+import {
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  PerspectiveCamera,
+  Scene,
+  WebGLRenderer,
+} from 'three'
+import { pick, pipe, difference, equals } from 'ramda'
 
-const angles = compose(toFloat, pick(['rx', 'ry', 'rz']))
+const angles = pipe(
+  pick(['rx', 'ry', 'rz']),
+  toFloat,
+  ({ rx, ry, rz }) => ({ x: rx, y: ry, z: rz })
+)
 
-class Model extends React.PureComponent {
+class Model extends React.Component {
   constructor(props) {
     super(props)
+    const rotation = angles(props)
     this.canvas = React.createRef()
-    this.state = createInitialState(this.props.size.width, 500)
+    this.state = initializeState(this.props.size.width, 500, rotation)
+    this.tween$ = createTransition$(rotation)
+      .do(r => console.log('ρ', r))
   }
 
   componentDidMount() {
     const { width } = this.props.size
-    const renderer = new WebGLRenderer({ alpha: true, antialias: true, canvas: this.canvas.current })
-    renderer.setSize(width, 500)
-    console.log('%c componentDidMount', 'color: darkred', renderer)
-    this.setState({ renderer })
+    this.renderer = new WebGLRenderer({
+      alpha: true,
+      antialias: true,
+      canvas: this.canvas.current,
+    })
+    this.renderer.setSize(width, 500)
+    this.subscription = this.tween$.subscribe(rotation =>
+      this.setState({ rotation })
+    )
   }
 
-  componentDidUpdate() {
-    console.log('%c componentDidUpdate', 'color: crimson', this.canvas)
+  componentDidUpdate(prevProps, prevState) {
+    const rotation = angles(this.props)
+    const prevRotation = angles(prevProps)
+    if (equals(rotation, prevRotation)) return
+    this.tween$.next(rotation)
   }
 
   componentWillUnmount() {
-    console.log('%c componentWillUnmount', 'color: red', this.canvas)
+    this.subscription.unsubscribe()
   }
 
   render() {
-    const { scene, camera, renderer, mesh } = this.state
-    const { rx, ry, rz } = angles(this.props)
-    if (mesh) Object.assign(mesh.rotation, {x:rx, y:ry, z:rz})
-    if (renderer) renderer.render(scene, camera)
+    const { scene, camera, mesh, rotation } = this.state
+    if (mesh) Object.assign(mesh.rotation, rotation)
+    if (this.renderer) this.renderer.render(scene, camera)
     return <canvas className={style.canvas} ref={this.canvas} />
   }
 }
 
 export default sizeMe()(Model)
 
-function createInitialState(width, height) {
+function initializeState(width, height, rotation) {
   const scene = new Scene()
   const camera = new PerspectiveCamera(75, width / height)
   const geometry = new BoxGeometry(1, 1, 1)
   const material = new MeshBasicMaterial({ color: 0xff0000 })
   const mesh = new Mesh(geometry, material)
-  mesh.position.z = -5
+  mesh.position.z = -2
   scene.add(mesh)
-  return { scene, mesh, camera }
+  return { scene, mesh, camera, rotation }
 }
