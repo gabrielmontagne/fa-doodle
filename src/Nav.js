c8eb2a9da2f616de085cbf8620600b79fb0234d6
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-10 21:03:00 +0000

    manually configure some povs.

--- src/Nav.js
+++ src/Nav.js
@@ -2,9 +2,44 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 import style from './Nav.module.css'
 
-export default function Nav({favCurves}) {
-  return <nav className={style.nav}>
-    Other curves,
-    { favCurves.map((path, i) => <NavLink className={style.link} activeClassName={style.active} key={path} to={path}>{path}</NavLink>) }
-  </nav>
+export default function Nav({ favCurves }) {
+  return (
+    <nav className={style.nav}>
+      Some curves,
+      {[
+        '/curve/0.9/0.2/0.1',
+        '/curve/0.1/5.0/3.0',
+        '/curve/0.0/0.2/0.1',
+        '/curve/0.9/0.2/0.2',
+        '/curve/0.1/0.8/0.8',
+        '/curve/1.0/0.3/0.0',
+        '/curve/0.6/0.9/0.1',
+        '/curve/0.3/0.5/0.6',
+      ].map(toLink)}
+      — Or perhaps load any of the following meshes,
+      {[
+        '/mesh/table/0/0/0',
+        '/mesh/house/0/0/0',
+        '/mesh/house/-45/10/15',
+        '/mesh/house/-80/-20/0',
+        '/mesh/louis-xiv/-40/15/0',
+        '/mesh/louis-xiv/0/0/0',
+        '/mesh/mercure/0/0/0',
+        '/mesh/mercure/30/-30/10',
+      ].map(toLink)}
+    </nav>
+  )
+}
+
+function toLink(path) {
+  return (
+    <NavLink
+      className={style.link}
+      activeClassName={style.active}
+      key={path}
+      to={path}
+    >
+      {path}
+    </NavLink>
+  )
 }
