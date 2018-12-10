fad87b732bcee2d276e8f0e287bce1236e801792
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-10 21:24:20 +0000

    minor style and layout tweaks

--- src/Nav.js
+++ src/Nav.js
@@ -5,23 +5,22 @@ import style from './Nav.module.css'
 export default function Nav({ favCurves }) {
   return (
     <nav className={style.nav}>
-      Some curves,
+      Click on the following links to see some curves,
       {[
         '/curve/0.9/0.2/0.1',
         '/curve/0.1/5.0/3.0',
         '/curve/0.0/0.2/0.1',
         '/curve/0.9/0.2/0.2',
         '/curve/0.1/0.8/0.8',
-        '/curve/1.0/0.3/0.0',
-        '/curve/0.6/0.9/0.1',
         '/curve/0.3/0.5/0.6',
       ].map(toLink)}
       — Or perhaps load any of the following meshes,
       {[
-        '/mesh/table/0/0/0',
         '/mesh/house/0/0/0',
         '/mesh/house/-45/10/15',
         '/mesh/house/-80/-20/0',
+        '/mesh/table/0/0/0',
+        '/mesh/table/20/-10/4',
         '/mesh/louis-xiv/-40/15/0',
         '/mesh/louis-xiv/0/0/0',
         '/mesh/mercure/0/0/0',
