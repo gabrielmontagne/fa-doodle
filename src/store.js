e5298103d4d770fb744d9345efd94ee27fa0aa48
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-20 05:39:17 +0000

    restore store structure

--- src/store.js
+++ src/store.js
@@ -1,6 +1,17 @@
-import { of } from 'rxjs'
+import { of, merge } from 'rxjs'
+import { catchError, scan, distinctUntilChanged, publishReplay, refCount } from 'rxjs/operators'
 import { Map } from 'immutable'
 
-const store$ = of(Map({ error: 'X-P' }))
+const noise$ = of(Map({ error: 'X-P' }))
+const model$ = of(state => state.set('okaloka', 666))
+
+const store$ = merge(noise$, model$)
+  .pipe(
+    catchError(error => of(state => state.set('error', error))),
+    scan((state, reduce) => reduce(state)),
+    distinctUntilChanged((a, b) => a.equals(b)),
+    publishReplay(1),
+    refCount()
+  )
 
 export default store$
