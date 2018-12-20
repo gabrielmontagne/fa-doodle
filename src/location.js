b44aa95839ab31a70d309b46151b6d77c4ec4f61
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-20 05:42:19 +0000

    sketch "local" location

--- src/location.js
+++ src/location.js
@@ -1,14 +1,17 @@
 import log from './caballo-vivo/log'
-import { createLocation$ } from './caballo-vivo/location'
-import { showNoise$, showModel$ } from './intents'
 import { OrderedMap } from 'immutable'
+import { createLocation$ } from './caballo-vivo/location'
+import { map, tap } from 'rxjs/operators'
+import { showNoise$, showModel$ } from './intents'
 
 const pathToIntent = OrderedMap([
   ['/curve/:h/:v/:u', coords => showNoise$.next(coords)],
   ['/mesh/:mesh/:rx/:ry/:rz', coords => showModel$.next(coords)],
 ])
-const location$ = createLocation$(pathToIntent)
-  .do(log('Location'))
-  .map(location => state => state.set('location', location))
+
+const location$ = createLocation$(pathToIntent).pipe(
+  tap(log('Location')),
+  map(location => state => state.set('location', location))
+)
 
 export default location$
--- src/store.js
+++ src/store.js
@@ -1,11 +1,12 @@
 import { of, merge } from 'rxjs'
 import { catchError, scan, distinctUntilChanged, publishReplay, refCount } from 'rxjs/operators'
 import { Map } from 'immutable'
+import location$ from './location'
 
 const noise$ = of(Map({ error: 'X-P' }))
 const model$ = of(state => state.set('okaloka', 666))
 
-const store$ = merge(noise$, model$)
+const store$ = merge(noise$, model$, location$)
   .pipe(
     catchError(error => of(state => state.set('error', error))),
     scan((state, reduce) => reduce(state)),
