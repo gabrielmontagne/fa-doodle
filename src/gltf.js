95a67124b629c8780a6be89e126a007e8786248a
gabriel montagné láscaris-comneno <gabriel@tibas.london>
2018-12-21 22:39:31 +0000

    migrate gltf to rxjs 6

--- src/gltf.js
+++ src/gltf.js
@@ -1,7 +1,8 @@
 import GLTFLoader from 'three-gltf-loader'
 import log from './caballo-vivo/log'
 import { fromJS, Map } from 'immutable'
-import { Observable } from 'rxjs'
+import { Observable, concat, of } from 'rxjs'
+import { tap, switchMap, map } from 'rxjs/operators'
 import { createNavigateTo$ } from './caballo-vivo/location'
 import { format } from 'd3-format'
 import { showModel$ } from './intents'
@@ -9,30 +10,37 @@ import { showModel$ } from './intents'
 const formatProgress = format('.1f')
 let modelCache = Map()
 
-const model$ = showModel$.switchMap(({ mesh, rx, ry, rz }) => {
-  return modelCache.has(mesh)
-    ? Observable.concat(
-        Observable.of(modelCache.get(mesh))
-          .do(log('Restore cached model'))
-          .map(model => state => state.setIn(['models', mesh], model)),
-        createNavigateTo$(`/mesh/${mesh}/${rx}/${ry}/${rz}`)
-      )
-    : Observable.concat(
-        Observable.of(state =>
-          state.set('loading', `model ‘${mesh}’`)
-        ),
-        createNavigateTo$(`/mesh/${mesh}/${rx}/${ry}/${rz}`),
-        createLoader$(`${process.env.PUBLIC_URL}/models/${mesh}/scene.gltf`)
-          .do(({ result }) => (modelCache = modelCache.set(mesh, result)))
-          .do(log('Model loader'))
-          .map(({ progress, result }) => state =>
-            state
-              .set('loading', `model ‘${mesh}’ (${formatProgress(progress)})`)
-              .setIn(['models', mesh], result)
+const model$ = showModel$.pipe(
+  switchMap(({ mesh, rx, ry, rz }) => {
+    return modelCache.has(mesh)
+      ? concat(
+          of(modelCache.get(mesh)).pipe(
+            tap(log('Restore cached model')),
+            map(model => state => state.setIn(['models', mesh], model))
           ),
-          Observable.of(state => state.delete('loading'))
-      )
-})
+          createNavigateTo$(`/mesh/${mesh}/${rx}/${ry}/${rz}`)
+        )
+      : concat(
+          of(state => state.set('loading', `model ‘${mesh}’`)),
+          createNavigateTo$(`/mesh/${mesh}/${rx}/${ry}/${rz}`),
+          createLoader$(
+            `${process.env.PUBLIC_URL}/models/${mesh}/scene.gltf`
+          ).pipe(
+            tap(({ result }) => (modelCache = modelCache.set(mesh, result))),
+            tap(log('Model loader')),
+            map(({ progress, result }) => state =>
+              state
+                .set(
+                  'loading',
+                  `model ‘${mesh}’ (${formatProgress(progress)})`
+                )
+                .setIn(['models', mesh], result)
+            )
+          ),
+          of(state => state.delete('loading'))
+        )
+  })
+)
 
 export default model$
 
--- src/store.js
+++ src/store.js
@@ -2,11 +2,10 @@ import { of, merge } from 'rxjs'
 import { catchError, scan, distinctUntilChanged, publishReplay, refCount } from 'rxjs/operators'
 import { Map } from 'immutable'
 import location$ from './location'
+import model$ from './gltf'
 import noise$ from './noise'
 
-const model$ = of(state => state.set('okaloka', 666))
-
-const store$ = merge(noise$, location$)
+const store$ = merge(noise$, model$, location$)
   .pipe(
     catchError(error => of(state => state.set('error', error))),
     scan((state, reduce) => reduce(state), Map()),
